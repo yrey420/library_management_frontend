9d8a9d04dccdb7c9c75b4b5d073a3152f651e2b9
yeison.sepulveda@advantage.com.co <yrey415@unab.edu.co>
2025-06-15 12:24:22 -0500

    Re instalacion de Angular, problemas de version

--- library-management/README.md
+++ library-management/README.md
@@ -1,4 +1,4 @@
-# AngularUseitTest
+# LibraryManagement
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.0.2.
 
--- library-management/src/app/app.ts
+++ library-management/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'angular-useit-test';
+  protected title = 'library-management';
 }
